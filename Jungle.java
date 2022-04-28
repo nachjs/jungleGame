7cbc6f48e23c7df7420baebb68d5c3bd110cc24e
ccrystallee <lee.crystal2006@gmail.com>
2022-04-28 12:40:10 -0700

    finished

--- Jungle.java
+++ Jungle.java
@@ -5,9 +5,10 @@ public class Jungle
 {
     public static void story(Character c){
 
-    }
+    } 
     public static void main(String args[])
     {
+        System.out.println("hi");
         Scanner scan = new Scanner(System.in);
     }
 }
