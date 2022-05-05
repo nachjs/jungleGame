634d1240ed35cacfa449ca9455f4cc05dc0a4c73
njoshi47 <100874580+njoshi47@users.noreply.github.com>
2022-05-05 12:06:26 -0700

    new

--- Tools.java
+++ Tools.java
@@ -1,3 +1,23 @@
-public interface Tools {
+public class Tools implements Interactives
+{
+    private boolean attack = false;
+    private int heartRemoval = 0;
     
+    public Tools( boolean myAttack, int myHeartRemoval)
+    {
+        attack = myAttack;
+        heartRemoval = myHeartRemoval;
+    }
+
+    @Override
+    public void act()
+    {
+        if ( )
+        
+    }
+
+    public int getDamage()
+    {
+        return heartRemoval;
+    }
 }
